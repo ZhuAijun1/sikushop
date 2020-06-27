098b9e08e664cc1e714066f9268d83d3bf7a71fa
ZhuAijun <496843103@qq.com>
2020-06-27 23:32:28 +0800

    test4

--- dist/js/shopdata.js
+++ dist/js/shopdata.js
@@ -6,26 +6,61 @@ $(function(){
     //     console.log(data)
     // })
     // 获取商品信息
-    $.get("http://jx.xuzhixiang.top/ap/api/productlist.php", {
-        uid: 34256
-    }, data => {
-        var array = data.data
-        console.log(array)
-        var str = ''
-        for(let i = 0;i< array.length;i++){
-            str +=
-            `<a href="detail.html?id=${array[i].pid}">
-            <li>
-                <img src="${array[i].pimg}" alt="">
-                <div class="jingying"><span>自营</span><span>直降</span></div>
-                <div class="name-shop">${array[i].pdesc}</div>
-                <div class="pprice">￥${array[i].pprice}</div>
-            </li>
-            </a>
-            `
+    function showlist(x){
+        $.get("http://jx.xuzhixiang.top/ap/api/productlist.php", {
+            pagesize:12,
+            pagenum:x,
+            uid: 34256,
+        }, data => {
+            var array = data.data
+            var str = ''
+            for(let i = 0;i< array.length;i++){
+                str +=
+                `<a href="detail.html?id=${array[i].pid}">
+                <li>
+                    <img src="${array[i].pimg}" alt="">
+                    <div class="jingying"><span>自营</span><span>直降</span></div>
+                    <div class="name-shop">${array[i].pdesc}</div>
+                    <div class="pprice">￥${array[i].pprice}</div>
+                </li>
+                </a>
+                `
+            }
+            $(".xiezi").html(str)
+            // console.log(data);
+        })
+    }
+    showlist(0)
+    $(".first").click(function(){
+        $(this).css({background:" #e93b39",color:" #fff"}).siblings().css({background:"white",color:"black"})
+        var pagenum = Number($(this).html()) - 1
+        showlist(pagenum)
+    })
+    $(".second").click(function(){
+        $(this).css({background:" #e93b39",color:" #fff"}).siblings().css({background:"white",color:"black"})
+        var pagenum = Number($(this).html()) - 1
+        console.log(pagenum)
+        showlist(pagenum)
+    })
+    var pageNum = 0
+    $(".shopdata .remai .yema .next").click(function(){
+        $(this).css({background:" #e93b39",color:" #fff"}).siblings().css({background:"white",color:"black"})
+        pageNum++
+        if(pageNum>1){
+            pageNum = 1
         }
-        $(".xiezi").html(str)
-        console.log(data);
+        showlist(pageNum)
+    })
+    $(".shopdata .remai .yema .prev").click(function(){
+        $(this).css({background:" #e93b39",color:" #fff"}).siblings().css({background:"white",color:"black"})
+        pageNum--
+        if(pageNum<0){
+            pageNum = 0
+        }
+        showlist(pageNum)
+    })
+    $(".shopdata .remai .gediao li").click(function(){
+        $(this).css({background:" #e93b39",color:" #fff"}).siblings().css({background:"white",color:"black"})
     })
     // 删除商品
     // $.get("http://jx.xuzhixiang.top/ap/api/goods/goods-delete.php",{
